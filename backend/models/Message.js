674b61ba66056fdb4ac2b133cf1e9793c3f8a235
Naftal-git <naftalbosire1206@gmail.com>
2024-11-23 18:23:53 +0300

    error

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -6,10 +6,9 @@ const messageSchema = new mongoose.Schema({
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-  read: { type: Boolean, default: false },
 });
 
 
-messageSchema.index({ sender: 1, recipient: 1, timestamp: -1 });
+// messageSchema.index({ sender: 1, recipient: 1, timestamp: -1 });
 
 module.exports = mongoose.model('Message', messageSchema);
