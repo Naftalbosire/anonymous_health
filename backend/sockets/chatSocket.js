e74a314ec7948520bcd765ecd5312bb4e6835c66
Naftal-git <naftalbosire1206@gmail.com>
2024-11-23 17:55:40 +0300

    socket log

--- backend/sockets/chatSocket.js
+++ backend/sockets/chatSocket.js
@@ -13,17 +13,17 @@ module.exports = (io) => {
     socket.on("private_message", async ({ sender, recipient, content }) => {
       try {
         const message = await Message.create({ sender, recipient, content });
-
+        console.log(message.content);
         const recipientSocketId = userSocketMap.get(recipient);
         if (recipientSocketId) {
           io.to(recipientSocketId).emit("private_message", message);
+          console.log('sent');
         } else {
           console.log("Recipient is offline. Message saved to database.");
         }
-
         socket.emit("private_message", message); // Optionally emit to sender
       } catch (error) {
-        console.error("Error sending private message:", error);
+        console.log("Error sending private message:", error);
         socket.emit("error", "Failed to send message");
       }
     });
