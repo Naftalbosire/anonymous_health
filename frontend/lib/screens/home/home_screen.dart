239b9181fb0d2ecc13550cc8d767a7da417f9db3
Naftal-git <naftalbosire1206@gmail.com>
2024-11-25 16:27:09 +0300

    merge

--- frontend/lib/screens/home/home_screen.dart
+++ frontend/lib/screens/home/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+// home_screen
 
 class _HomeScreenState extends State<HomeScreen> {
   String greetingMessage = '';
